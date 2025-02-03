47474aa69d28c38c42bffb456752de65a94b22e1
MikaelGrace <MikeOkoduwa700@gmail.com>
2025-02-03 10:32:07 +0100

    add 16x16 grid of boxes using flexbox

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 256; i++) {
+    squareBox = document.createElement("div");
+    squareBox.style.width = "95px";
+    squareBox.style.height = "95px";
+    squareBox.style.border = "1px solid blue";
+    squareBox.style.margin = "10px";
+    squareBox.style.boxSizing = "border-box";
+    squareBox.style.borderRadius = "5px";
+    squareBox.classList.add("boxes");
+    squareBox.style.setProperty("--someColor");
+
+
+    container.appendChild(squareBox);
+    
+}
